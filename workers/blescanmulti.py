3794ac0c1ae029194395c79005e8c85a2547b91c
Alexsandroz <alexsandroz@gmail.com>
2025-04-07 14:16:44 +0100

    Add homeassistant config capability to blescanmulti

--- workers/blescanmulti.py
+++ workers/blescanmulti.py
@@ -1,6 +1,6 @@
 import time
 
-from mqtt import MqttMessage
+from mqtt import MqttMessage, MqttConfigMessage, MqttClient
 
 from workers.base import BaseWorker
 from utils import booleanize
@@ -19,6 +19,7 @@ class BleDeviceStatus:
         available: bool = False,
         last_status_time: float = None,
         message_sent: bool = True,
+        haas_config_sent: bool = False,
     ):
         if last_status_time is None:
             last_status_time = time.time()
@@ -29,10 +30,11 @@ class BleDeviceStatus:
         self.available = available
         self.last_status_time = last_status_time
         self.message_sent = message_sent
+        self.hass_config_sent = haas_config_sent
 
-    def set_status(self, available):
-        if available != self.available:
-            self.available = available
+    def set_status(self, scanEntry):
+        self.available = scanEntry is not None
+        if self.available:
             self.last_status_time = time.time()
             self.message_sent = False
 
@@ -52,8 +54,67 @@ class BleDeviceStatus:
         else:
             return self.worker.unavailable_payload
 
+    def payload_hass_config_device_tracker(self, device):
+        ret =  '{'
+        ret += '"dev":{'
+        ret += '"ids":["{}"],'.format(self.name)
+        ret += '"cns":[["mac","{}"]],'.format(self.mac)
+        ret += '"name":"{}"'.format(self.name)
+        #ret += '"sw": "",'
+        #ret += '"mf": "",'
+        #ret += '"mdl": "",'
+        #ret += '"cu": ""'
+        ret += '},'
+        ret += '"name":"tracker",'
+        ret += '"~":"{}/{}/presence/{}",'.format(self.worker.global_topic_prefix, self.worker.topic_prefix, self.name)
+        ret += '"uniq_id":"{}_tracker",'.format(self.name)
+        ret += '"qos":1,'
+        ret += '"stat_t":"~",'
+        ret += '"avty_t": "~/LTW",'
+        ret += '"source_type":"bluetooth_le"'
+        ret += '}'
+        return ret
+
+    def payload_hass_config_rssi(self, device):
+        ret =  '{'
+        ret += '"dev":{'
+        ret += '"ids":["{}"],'.format(self.name)
+        ret += '"cns":[["mac","{}"]],'.format(self.mac)
+        ret += '"name":"{}"'.format(self.name)
+        #ret += '"sw": "",'
+        #ret += '"mf": "",'
+        #ret += '"mdl": "",'
+        #ret += '"cu": ""'
+        ret += '},'
+        ret += '"name":"rssi",'
+        ret += '"~":"{}/{}/presence/{}",'.format(self.worker.global_topic_prefix, self.worker.topic_prefix, self.name)
+        ret += '"uniq_id":"{}_rssi",'.format(self.name)
+        ret += '"qos":1,'
+        ret += '"dev_cla":"signal_strength",'
+        ret += '"stat_t":"~/rssi",'
+        ret += '"unit_of_meas":"dBm",'
+        ret += '"entity_category":"diagnostic",'
+        ret += '"avty_t": "~/LTW",'
+        ret += '"stat_cla":"measurement",'
+        ret += '"source_type":"bluetooth_le"'
+        ret += '}'
+        return ret
+
     def generate_messages(self, device):
         messages = []
+        if not self.hass_config_sent and self.available:
+            self.hass_config_sent = True
+            #MqttClient.mqttc.will_set(
+            #            topic=self.worker.format_topic("presence/{}".format(self.name)),
+            #            payload=MqttClient.LWT_ONLINE, retain=True)
+            messages.append(
+                MqttConfigMessage("homeassistant", "sensor/{}_rssi".format(self.name),
+                    payload=self.payload_hass_config_rssi(device), retain=True)
+            )
+            messages.append(
+                MqttConfigMessage("homeassistant", "device_tracker/{}".format(self.name),
+                    payload=self.payload_hass_config_device_tracker(device), retain=True)
+            )
         if not self.message_sent and self.has_time_elapsed():
             self.message_sent = True
             messages.append(
@@ -70,9 +131,17 @@ class BleDeviceStatus:
                         ),
                         payload=device.rssi,
                     )
-                )
+                )            
+                messages.append(
+                    MqttMessage(
+                        topic=self.worker.format_topic(
+                            "presence/{}/{}".format(self.name, "LTW")
+                        ),
+                        payload="online", retain=True,
+                    )
+                )            
         return messages
-
+        
 
 class BlescanmultiWorker(BaseWorker):
     # Default values
@@ -114,15 +183,15 @@ class BlescanmultiWorker(BaseWorker):
         ret = []
 
         try:
-            devices = self.scanner.scan(
+            scanEntries = self.scanner.scan(
                 float(self.scan_timeout), passive=booleanize(self.scan_passive)
             )
-            mac_addresses = {device.addr: device for device in devices}
+            mac_addresses = {scanEntry.addr: scanEntry for scanEntry in scanEntries}
 
             for status in self.last_status:
-                device = mac_addresses.get(status.mac, None)
-                status.set_status(device is not None)
-                ret += status.generate_messages(device)
+                scanEntry = mac_addresses.get(status.mac, None)
+                status.set_status(scanEntry)
+                ret += status.generate_messages(scanEntry)
 
         except btle.BTLEException as e:
             logger.log_exception(
